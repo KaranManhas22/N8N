450f1809c5d50d5e8e0ad1742860ad4b6c92cfb4
Ubuntu <ubuntu@ip-172-31-12-136.ap-south-1.compute.internal>
2026-03-23 07:45:40 +0000

    singh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Karansw Node App Running 🚀');
+  res.send('Karanswh Node App Running 🚀');
 });
 
 app.listen(5000, () => {
